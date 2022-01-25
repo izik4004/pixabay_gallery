032f071e963fc3924f8fb07b34ea4f24f946c9f9
izik4004 <izik4004@gmail.com>
2022-01-26 00:33:57 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purplr-500 text-xl mb-2">
+            Photo by John Doe
+        </div>
+        <ul>
+            <li>
+              <strong>Views:</strong>
+              400
+            </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
